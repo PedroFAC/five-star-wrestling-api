17872f334ac4373d5f5f7b7e8d70f99864a66392
PedroFAC <pedrofacpeix@gmail.com>
2021-10-15 08:57:11 -0300

    Added cors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { getMatches } = require("./services");
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.get("/", getMatches);
 
 app.listen(process.env.PORT || 3000);
